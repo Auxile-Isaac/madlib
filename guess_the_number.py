2e0b679c85af34791b87a2344ba67fcda1bcf235
Auxile_Isaac <auxile.isaac@gmail.com>
2022-08-15 16:36:28 +0200

    computer guess number added

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,7 +1,8 @@
 import random
 
+
 def guess(x):
-    random_number = random.randint(1,x)
+    random_number = random.randint(1, x)
     user_guess = 0
     while user_guess != random_number:
         user_guess = int(input(f"guess a number between 1 and {x}: "))
@@ -10,5 +11,6 @@ def guess(x):
         elif user_guess > random_number:
             print("sorry guess again. too high")
     print(f"woow you guessed the correct number {user_guess}")
+
 
 guess(10)
